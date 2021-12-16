90e94cb316fa1bae3f6afa0d794a4ccc76ebde1d
Kevin Herrera <1herrerake@gmail.com>
2021-12-15 18:38:19 -0600

    tailwind css setup. login and sign up page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+const Login = lazy(() => import("./pages/login"));
+const Signup = lazy(() => import("./pages/sign-up"));
+
 function App() {
   return (
-    <p>iightbet</p>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path={ROUTES.LOGIN} element={<Login />} />
+          <Route path={ROUTES.SIGN_UP} element={<Signup />} />
+        </Routes>
+      </Suspense>
+    </Router>
   );
 }
 
